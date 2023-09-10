47c2415f5a7abf6a31c3bf99535cd8fbf6caff08
Ranojoy Banerjee <ronban.14@gmail.com>
2023-09-10 19:08:37 +0530

    Only one item can be added.

--- Food Delivery App/App.java
+++ Food Delivery App/App.java
@@ -1,35 +1,49 @@
-/**
- * app
- */
-
-
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
 
     public static void main(String[] args) {
 
-        String menu[] = {"Pizza","Sandwich","Coffee","Burger"};
-        int cost[] = {400,200,50,100};
-
+        HashMap<String,Integer> Menu = new HashMap<>();
+        
+        Menu.put("PIZZA", 400);
+        Menu.put("SANDWICH", 200);
+        Menu.put("COFFEE", 50);
+        Menu.put("BURGER", 100);
+        
         System.out.println("Menu\n");
         System.out.println("Item" + "   " + "Price");
-
-        for(int i = 0; i < menu.length; i++){
-            System.out.println(menu[i] + "   " + cost[i]);
+        
+        for(Map.Entry<String, Integer> e : Menu.entrySet()){
+            System.out.println(e.getKey() + "   " + e.getValue());
         }
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nWhat do you want to order?");
-        String item =  input.nextLine();
+        String item =  (input.nextLine()).toUpperCase();
+
+        int cost = 0;
+
+        if(Menu.containsKey(item)){
+            cost = Menu.get(item);
+        }
+        
+        System.out.println("\nEnter the quantity.");
+        int quantity =  input.nextInt();
         
         System.out.println("\nHow far are you?");
         double dist =  input.nextDouble();
 
-        Order o1 = new Order(item, dist);
-        System.out.println("You have ordered "+ o1.food + ", and you are "+ o1.distance + "kms. away");
+        Order o1 = new Order(item, cost, quantity, dist);
+        System.out.println("You have ordered "+ o1.quantity+ " " + o1.food + "(s), and you are "+ o1.distance + " kms. away");
 
+        
+        System.out.println(o1.makeBill());
+
+        
 
         input.close();
 
--- Food Delivery App/Order.java
+++ Food Delivery App/Order.java
@@ -1,9 +1,86 @@
-public class Order{
+public class Order {
     String food;
+    int price;
+    int quantity;
     double distance;
+
+    Order(String $item, int $price, int $quantity, double $dist) {
+        food = $item;
+        price = $price;
+        quantity = $quantity;
+        distance = $dist;
+    }
+
+    double makeBill() {
+        int pre_total = price * quantity;
+        double delCharge = 0;
+        double total;
+
+        if (200 < pre_total && pre_total <= 500) {
+
+            if (0 < distance && distance <= 3.00) {
+                delCharge = 40;
+            } else if (5.01 < distance && distance <= 10.00) {
+                delCharge = 100;
+            } else if (10.01 < distance && distance <= 15.00) {
+                delCharge = 150;
+            } else if (15.01 < distance && distance <= 20.00) {
+                delCharge = 200;
+            } else if (20.01 < distance && distance <= 25.00) {
+                delCharge = 250;
+            } else if (25.01 < distance && distance <= 99.00) {
+                delCharge = 300;
+            }
+        } else if (501 < pre_total && pre_total <= 1000) {
+
+             if (5.01 < distance && distance <= 10.00) {
+                delCharge = 50;
+            } else if (10.01 < distance && distance <= 15.00) {
+                delCharge = 100;
+            } else if (15.01 < distance && distance <= 20.00) {
+                delCharge = 150;
+            } else if (20.01 < distance && distance <= 25.00) {
+                delCharge = 200;
+            } else if (25.01 < distance && distance <= 99.00) {
+                delCharge = 250;
+            }
+        } else if (1001 < pre_total && pre_total <= 1500) {
+
+            if (10.01 < distance && distance < 15.00) {
+                delCharge = 50;
+            } else if (15.01 < distance && distance <= 20.00) {
+                delCharge = 10;
+            } else if (20.01 < distance && distance <= 25.00) {
+                delCharge = 150;
+            } else if (25.01 < distance && distance <= 99.00) {
+                delCharge = 200;
+            }
+        } else if (1501 < pre_total && pre_total <= 2000) {
+            if (15.01 < distance && distance <= 20.00) {
+                delCharge = 50;
+            } else if (20.01 < distance && distance <= 25.00) {
+                delCharge = 100;
+            } else if (25.01 < distance && distance <= 99.00) {
+                delCharge = 150;
+            }
+        } else if (2001 < pre_total && pre_total <= 2500) {
+
+            if (20.01 < distance && distance <= 25.00) {
+                delCharge = 50;
+            } else if (25.01 < distance && distance <= 99.00) {
+                delCharge = 100;
+            }
+        } else if (2501 < pre_total && pre_total <= 3000) {
+            if (25.01 < distance && distance <= 99.00) {
+                delCharge = 50;
+            }
+        } else if (3001 < pre_total) {
+            delCharge = 0;
+        }
     
-    Order(String item, double dist){
-        food = item;
-        distance = dist;
+
+        total = pre_total + delCharge;
+
+        return total;
     }
 }
